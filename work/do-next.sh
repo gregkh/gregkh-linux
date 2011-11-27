11e46ce56bcc0866e651afb6628109800e641779
Greg Kroah-Hartman <gregkh@suse.de>
2011-11-28 06:23:07 +0900

    fix up do scripts to handle online/offline issues

--- work/do-next.sh
+++ work/do-next.sh
@@ -7,7 +7,17 @@ TREE=`basename ${PWD}`
 git fp ${TREE}-next &&				\
 	../added-to-${TREE}-next 0*.patch &&	\
 	git checkout ${TREE}-next && 		\
-	git merge work &&			\
-	git push kroah.com &&			\
-	git push &&				\
-	git checkout work
+	git merge work
+
+# if something goes wrong, exit
+if [ $? -ne 0 ] ; then
+	exit
+fi
+
+# Only push if we have a network connection
+NM_STATUS=`nmcli -terse -f state nm`
+if [ "$NM_STATUS" = "connected" ] ; then
+	git push kroah.com && git push
+fi
+
+git checkout work
--- work/do.sh
+++ work/do.sh
@@ -6,7 +6,17 @@ TREE=`basename ${PWD}`
 git fp ${TREE}-linus &&				\
 	../added-to-${TREE} 0*.patch &&		\
 	git checkout ${TREE}-linus && 		\
-	git merge work &&			\
-	git push kroah.com &&			\
-	git push &&				\
-	git checkout work
+	git merge work
+
+# if something goes wrong, exit
+if [ $? -ne 0 ] ; then
+	exit
+fi
+
+# Only push if we have a network connection
+NM_STATUS=`nmcli -terse -f state nm`
+if [ "$NM_STATUS" = "connected" ] ; then
+	git push kroah.com && git push
+fi
+
+git checkout work
