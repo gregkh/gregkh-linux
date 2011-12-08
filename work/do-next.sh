bdc2e19f7876a6d5d6e1f3f3ec244a2dfcda72a1
Greg Kroah-Hartman <gregkh@suse.de>
2011-12-08 11:43:52 -0800

    work/do-next.sh: update to work on machines not using network manager

--- work/do-next.sh
+++ work/do-next.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+#
+# Take the branch we are working on, create the patches, email out that we
+# applied them, merge to the branch, and push the branch to the servers if we
+# are online.
+
 
 PWD=`pwd`
 TREE=`basename ${PWD}`
+
+# determine if we are online or not, depending on the machine
+online()
+{
+	ONLINE=0
+
+	# some machines I don't use network manager, so handle them
+	HOSTNAME=`hostname -s`
+	if [ "$HOSTNAME" = "clark" ] ; then
+		ONLINE=1
+	fi
+
+	# check network manager to see if we are online
+	NM_STATUS=`nmcli -terse -f state nm`
+	if [ "$NM_STATUS" = "connected" ] ; then
+		ONLINE=1
+	fi
+}
+
+
 
 #git fp tty-next && ../added-to-tty-next 0*.patch && git checkout tty-next && git merge work && git push && git checkout work
 git fp ${TREE}-next &&				\
@@ -14,9 +39,10 @@ if [ $? -ne 0 ] ; then
 	exit
 fi
 
+online
+
 # Only push if we have a network connection
-NM_STATUS=`nmcli -terse -f state nm`
-if [ "$NM_STATUS" = "connected" ] ; then
+if [ "$ONLINE" = "1" ] ; then
 	git push kroah.com && git push
 fi
 
