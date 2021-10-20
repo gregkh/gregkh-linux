305da9e90de944af168a713f502a34ce0698ac00
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-10-20 10:39:54 +0200

    tuxbuild: 5.14 is latest kernel

--- stable/tuxbuild/run_all.sh
+++ stable/tuxbuild/run_all.sh
@@ -2,6 +2,6 @@
 
 ~/.local/bin/tuxsuite build-set \
     --git-repo 'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git' \
-    --git-ref queue/5.11 \
+    --git-ref queue/5.14 \
     --tux-config all.yaml \
     --set-name all
