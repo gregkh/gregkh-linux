10a0b8b5e73b2105ef19ab4ca62bf16d0d104ff9
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2022-01-24 18:11:04 +0100

    tuxbuild updates

--- stable/tuxbuild/run_one.sh
+++ stable/tuxbuild/run_one.sh
@@ -2,6 +2,6 @@
 
 ~/.local/bin/tuxsuite build-set \
     --git-repo 'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git' \
-    --git-ref queue/5.14 \
+    --git-ref queue/5.15 \
     --tux-config oneshot.yaml \
     --set-name one_x86
--- stable/tuxbuild/run_x86.sh
+++ stable/tuxbuild/run_x86.sh
@@ -2,6 +2,6 @@
 
 ~/.local/bin/tuxsuite build-set \
     --git-repo 'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git' \
-    --git-ref queue/5.14 \
+    --git-ref queue/5.15 \
     --tux-config x86.yaml \
     --set-name x86
