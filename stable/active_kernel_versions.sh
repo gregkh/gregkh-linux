feec5418b931866be7e4b8bf2dd634a208f90ff8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2023-08-31 12:21:00 +0200

    stable: move a bunch of scripts into the stable-queue tree.
    most of this all was duplicated so remove dups.

--- stable/active_kernel_versions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for VERSION in $(cat /home/gregkh/linux/stable/active_kernel_versions); do
-	echo -n "${VERSION} "
-done
-echo ""
-
--- stable/active_kernel_versions_get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-VERSIONS=$(/home/gregkh/linux/stable/active_kernel_versions.sh)
-V=$(gum choose --no-limit ${VERSIONS})
-echo "${V}"
-exit
